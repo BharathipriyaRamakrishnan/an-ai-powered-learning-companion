ed430a07608ac487562cbeb47de11bb6c86022ea
BharathipriyaRamakrishnan <bharathipriyaramakrishnan@gmail.com>
2025-05-27 17:53:05 +0530

    Implement UI dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,49 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
+// import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SAAS app</h1>
-      <Button>
-        Lets get started
-      </Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className='home-section'>
+            <CompanionCard
+              id="123"
+              name="Neura the Brainy Explorer"
+              topic="Neural Network of the Brain"
+              subject="science"
+              duration={45}
+              color="#ffda6e"
+            />
+            <CompanionCard
+              id="456"
+              name="Counsy the Number Wizard"
+              topic="Derivatives & Integrals"
+              subject="mathematics"
+              duration={30}
+              color="#e5d0ff"
+            />
+            <CompanionCard
+              id="789"
+              name="Verba the vocublary builder"
+              topic="Language"
+              subject="English Literature"
+              duration={30}
+              color="#BDE7FF"
+            />
+      </section>
+      <section className='home-section'>
+          <CompanionsList 
+            title="Recently completed sessions"
+            companions={recentSessions}
+            classNames = "w-2/3 max-lg:w-full"
+          />
+          <CTA/>
+      </section>
+    </main>
   )
 }
 
