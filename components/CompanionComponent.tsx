730b111d55478b457e6bc56b1eaa29531c73c5ec
BharathipriyaRamakrishnan <bharathipriyaramakrishnan@gmail.com>
2025-05-28 12:31:27 +0530

    Implement Conversations and session history

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -7,6 +7,7 @@ import Lottie, { LottieRefCurrentProps } from 'lottie-react';
 import Image from 'next/image';
 import React, { useEffect, useRef, useState } from 'react'
 import soundWaves from '@/constants/soundwaves.json'
+import { addToSessionHistory } from '@/lib/actions/companion.actions';
 
 enum CallStatus {
     INACTIVE = 'INACTIVE',
@@ -36,7 +37,11 @@ const CompanionComponent = ({companionId, subject, topic, name, userName, userIm
     
     useEffect(()=> {
         const onCallStart = () => setcallStatus(CallStatus.ACTIVE);
-        const onCallEnd = () => setcallStatus(CallStatus.FINISHED);
+        const onCallEnd = () => {
+            setcallStatus(CallStatus.FINISHED);
+            addToSessionHistory(companionId)
+
+        }
         const onMessage = (message: Message) => {
             if(message.type === 'transcript' && message.transcriptType === 'final'){
                 const newMessage = { role: message.role, content: message.transcript}
